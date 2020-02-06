3774705ac004d56617cb17e31b3c9c0ab90a0f71
Siam <siam.tongpanyu@gmail.com>
2020-02-06 12:07:02 +0700

    Updates

---
+++ precache-manifest.751f97e7f5174a31d0aa2ccc4dcd036f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "966fb990f7fea73eedb554230af23443",
+    "url": "/siam-react/index.html"
+  },
+  {
+    "revision": "1f025bb6159c2551ad63",
+    "url": "/siam-react/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "236cbb0cd2929b687394",
+    "url": "/siam-react/static/css/main.b8963c73.chunk.css"
+  },
+  {
+    "revision": "1f025bb6159c2551ad63",
+    "url": "/siam-react/static/js/2.360752d5.chunk.js"
+  },
+  {
+    "revision": "e933f8dbfdf496cb72e495663a499a00",
+    "url": "/siam-react/static/js/2.360752d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "236cbb0cd2929b687394",
+    "url": "/siam-react/static/js/main.631f8608.chunk.js"
+  },
+  {
+    "revision": "3be004c5a9182f2bc85d",
+    "url": "/siam-react/static/js/runtime-main.cef894ee.js"
+  }
+]);
